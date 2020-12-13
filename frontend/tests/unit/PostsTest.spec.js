ccfcfc2ebc1aed70b6e220a52cb03bfa94dd6024
rauno99 <rauno.polluaar@gmail.com>
2020-12-13 21:57:05 +0200

    Fixed tests

--- frontend/tests/unit/PostsTest.spec.js
+++ frontend/tests/unit/PostsTest.spec.js
@@ -2,6 +2,7 @@ import {mount, createLocalVue} from '@vue/test-utils'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 import Posts from "../../src/components/Posts.vue";
+import moment from 'moment';
 
 const localVue = createLocalVue();
 
@@ -99,17 +100,18 @@ jest.mock("axios", () => ({
 describe('Posts', () => {
 
     const wrapper = mount(Posts, {router, store, localVue});
-    const posts = wrapper.findAll(".post");
 
     it('1 == 1', function () {
         expect(true).toBe(true)
     });
 
     it('All rendered', () => {
+        const posts = wrapper.findAll(".post");
         expect(posts.length).toEqual(testData.length);
     });
 
     it('Media rendered', () => {
+        const posts = wrapper.findAll(".post");
         for(let i = 0; i < posts.length; i++){
             let post = posts.at(i);
             let postTest = testData[i];
@@ -120,7 +122,7 @@ describe('Posts', () => {
                 }
 
                 if(postTest.media.type === "image"){
-                    expect(post.find('.post-image').find('image').exists()).toBe(true);
+                    expect(post.find('.post-image').find('img').exists()).toBe(true);
                 }
             }
             else{
@@ -130,11 +132,12 @@ describe('Posts', () => {
     });
 
     it('Timestamps are correct', () => {
+        const posts = wrapper.findAll(".post");
         for (let i = 0; i < posts.length; i++) {
             let post = posts.at(i);
             let postTest = testData[i];
             let correct = moment(postTest.createTime).format('LLLL');
-            expect(post.find('.post-author').find('small').text()).toEqual(correct)
+            expect(post.find('.post-author').findAll('small').at(1).text()).toEqual(correct)
         }
     });
 });
